ccaa280c0b530d6f8be30b3b3395b5c2225abcf5
OmarZakir <99466510+OmarZakir@users.noreply.github.com>
2022-04-01 10:56:39 +0530

    Delete VoizFonica directory

--- VoizFonica/src/main/java/com/iPrimed/VoizFonica/Postpaid/PostPaidRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Iprimed.VoizFonica;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface PostPaidRepository  extends JpaRepository<PostPaid, String> {
-	public PostPaid findByProductId(String productId);
-
-}
--- VoizFonica/src/main/java/com/iPrimed/VoizFonica/User/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iPrimed.VoizFonica;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
-	public User findByphno(long phno);
-}
